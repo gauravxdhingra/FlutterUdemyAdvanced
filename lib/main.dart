66ee2bebea9f8ebffe3e4e4d1e1521f715a2bbda
GauravXd <grvdhingra1999@gmail.com>
2019-11-21 21:31:16 +0530

    Adding Transaction Cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:app2/Transaction.dart';
 import 'package:flutter/material.dart';
+import 'Transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,18 +14,44 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: '1',
+      title: 'New Shoes',
+      amount: 50.5,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: '2',
+      title: 'Groceries',
+      amount: 100,
+      date: DateTime.now(),
+    )
+  ];
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter App"),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Card(
-            child: Text("Chart"),
+            child: Container(
+              width: double.infinity,
+              child: Text("Chart"),
+              color: Colors.deepOrange,
+            ),
+            elevation: 5,
           ),
-          Card(
-            child: Text("Expenses"),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                color: Colors.blue[200],
+                child: Text(tx.title),
+              );
+            }).toList(),
           ),
         ],
       ),
