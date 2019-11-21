b5e90ca8887d6c434d6c77a9ed3175467c276ab5
GauravXd <grvdhingra1999@gmail.com>
2019-11-21 20:09:05 +0530

    InitialStaging

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: Text("Chart"),
+          ),
+          Card(
+            child: Text("Expenses"),
+          ),
+        ],
+      ),
+    );
+  }
+}
