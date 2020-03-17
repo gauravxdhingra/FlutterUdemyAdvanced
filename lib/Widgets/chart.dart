daae11c6dcfc5df0c0351af804cd110a5c4279e5
GauravXd <grvdhingra1999@gmail.com>
2020-03-17 18:17:57 +0530

    Chart bars generated and mapped to values

--- lib/Widgets/chart.dart
+++ lib/Widgets/chart.dart
@@ -1,4 +1,5 @@
 import 'package:app2/Models/Transaction.dart';
+import 'package:app2/Widgets/chart_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -28,9 +29,18 @@ class Chart extends StatelessWidget {
         print(totalSum);
 
         return {
-          'day': DateFormat.E().format(weekDay).substring(0, 1),
+          'day': DateFormat.E().format(weekDay).substring(0, 2),
           'amount': totalSum,
         };
+      },
+    );
+  }
+
+  double get totalSpending {
+    return groupedTransactionValues.fold(
+      0.0,
+      (sum, item) {
+        return sum + item['amount'];
       },
     );
   }
@@ -42,8 +52,17 @@ class Chart extends StatelessWidget {
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: groupedTransactionValues.map((data) {
-          return Text('${data['day']} : ${data['amount']}');
+          return Flexible(
+            fit: FlexFit.tight,
+            child: Chartbar(
+                data['day'],
+                data['amount'],
+                totalSpending == 0.0
+                    ? 0.0
+                    : (data['amount'] as double) / totalSpending),
+          );
         }).toList(),
       ),
     );
